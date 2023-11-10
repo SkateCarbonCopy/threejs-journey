c63c911afb1ba38ed23625eba040d25a2720042a
Colin Gastelle <crgastelle@gmail.com>
2023-11-10 10:53:03 -0700

    Adding all 'Basics' projects

---
+++ 03/script.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+// Camera
+// full field of view would be 365, 75 is a lot but good for this practice - -most of the time the instructor is around 35
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+    // canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
